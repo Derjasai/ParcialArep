c8a3000668ed3fd20ab23a373a322f5be19b09a1
unknown <daniel.ramos-j@labinfo.is.escuelaing.edu.co>
2023-02-24 14:57:42 -0500

    se logra invocar un metodo sin parametros

--- src/main/java/edu/eci/arep/app/HttpServer.java
+++ src/main/java/edu/eci/arep/app/HttpServer.java
@@ -1,5 +1,8 @@
 package edu.eci.arep.app;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.*;
 import java.io.*;
 import java.util.Objects;
@@ -47,7 +50,6 @@ public class HttpServer {
                     outputLine = index();
                 }else{
                     outputLine = getQuery(request);
-                    System.out.println(outputLine);
                 }
             }else {
                 outputLine = index();
@@ -61,8 +63,65 @@ public class HttpServer {
         serverSocket.close();
     }
 
+
+
+    private static String getMethods(String clase){
+        String res = "";
+        try {
+            Class<?> c = Class.forName(clase);
+            Method[] methods = c.getMethods();
+            res += "METODOS: ";
+            for (Method method: methods) {
+                res += method.getName() + "\n";
+            }
+            res += "CAMPOS: \r\n";
+            Field[] fields = c.getFields();
+            for (Field field: fields) {
+                res += field.getName() + "\n";
+            }
+        } catch (ClassNotFoundException e) {
+            res = "No se encontró la clase";
+        }
+        return res;
+    }
+
+    private static String invokeMethod(String clase, String metodo){
+        System.out.println(clase + " "+metodo);
+        String res = "";
+        try {
+            Class<?> c = Class.forName(clase);
+            Method[] methods = c.getMethods();
+            Method m = c.getMethod(metodo);
+            m.invoke(null);
+            res = "Se invocó al metodo";
+
+        } catch (ClassNotFoundException e) {
+            res = "No se encontró la clase";
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        return res;
+    }
+
     private static String getQuery(String query){
-        return getHeader("text/html") + query.split("\\?")[1];
+        String res = query.split("\\?")[1];
+        res = res.split("=")[1];
+
+        if(query.contains("Class")){
+            res = (res.replace("Class","")).replace("(","").replace(")","");
+            res = getMethods(res);
+        } else if (query.contains("invoke")) {
+            res = (res.replace("invoke","")).replace("(","").replace(")","");
+            String clase = res.split(",")[0];
+            String metodo = res.split(",")[1];
+            res = invokeMethod(clase, metodo);
+
+        }
+        return getHeader("text") + res;
     }
 
     private static String getHeader(String type){
@@ -97,7 +156,7 @@ public class HttpServer {
                 "                    document.getElementById(\"getrespmsg\").innerHTML =\n" +
                 "                    this.responseText;\n" +
                 "                }\n" +
-                "                xhttp.open(\"GET\", \"/hello?name=\"+nameVar);\n" +
+                "                xhttp.open(\"GET\", \"/consulta?comando=\"+nameVar);\n" +
                 "                xhttp.send();\n" +
                 "            }\n" +
                 "        </script>\n" +
@@ -113,7 +172,7 @@ public class HttpServer {
                 "\n" +
                 "<script>\n" +
                 "            function loadPostMsg(name){\n" +
-                "                let url = \"/hellopost?name=\" + name.value;\n" +
+                "                let url = \"/consulta?comando=\" + name.value;\n" +
                 "\n" +
                 "                fetch (url, {method: 'POST'})\n" +
                 "                    .then(x => x.text())\n" +
